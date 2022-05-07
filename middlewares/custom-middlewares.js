eb990b5018387647f6fd339d907f9993aefeac01
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-07 15:29:59 +0600

    Created middleware to verify user access

--- middlewares/custom-middlewares.js
+++ middlewares/custom-middlewares.js
@@ -1,11 +1,37 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const verifyToken = (req, res, next) => {
+    const user = req.query?.user;
     const bearerHeader = req.headers.authorization;
-    if (!bearerHeader) {
-        res.status(401).send({ message: 'Unauthorized access' });
-    } else {
-        const bearerToken = bearerHeader.split(' ')[1];
-        req.token = bearerToken;
+
+    if (!user) {
         next();
+    } else {
+        if (!bearerHeader) {
+            return res.status(401).send({ message: 'Unauthorized access' });
+        } else {
+            const bearerToken = bearerHeader.split(' ')[1];
+            const decodedToken = jwt.verify(
+                bearerToken,
+                process.env.JWT_SECREAT_KEY,
+                (err, decoded) => {
+                    if (err) {
+                        return res
+                            .status(403)
+                            .send({ message: 'Forbidden access' });
+                    }
+
+                    return decoded;
+                }
+            );
+
+            if (decodedToken.user !== user) {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+
+            return next();
+        }
     }
 };
 
